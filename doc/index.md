b4e3a94bbd623673cacd4cc7188657e11ac0091f
Heribert Schütz <hs@webxcerpt.com>
2015-03-09 20:03:34 +0100

    added openCPQ code from private repo
    from git@git.assembla.com:opencpq.git, commit
    c0bb7ddb6298dc0b0101b9609cf839509d310118, with some adaptations to
    non-source files

---
+++ doc/index.md
@@ -0,0 +1,12 @@
+openCPQ Documentation
+=====================
+
+- Getting started
+- Installation
+- Tutorial
+  - How to model
+- Concepts
+- Types and Widgets
+- Extending openCPQ
+  - Writing your own types
+  - Modify existing types
---
+++ index.js
@@ -0,0 +1,24 @@
+require("babel/polyfill");
+
+module.exports = {
+	...require("./lib/base"),
+	...require("./lib/util"),
+	...require("./lib/primitives"),
+	...require("./lib/html"),
+	...require("./lib/path"),
+	...require("./lib/label"),
+	...require("./lib/group"),
+	...require("./lib/table"),
+	...require("./lib/select"),
+	...require("./lib/bom"),
+	...require("./lib/linear-aggregation"),
+	...require("./lib/quantification"),
+	...require("./lib/toc"),
+	...require("./lib/validation"),
+	...require("./lib/problems"),
+	...require("./lib/names"),
+	...require("./lib/op"),
+	...require("./lib/workbench"),
+	...require("./lib/root"),
+	...require("./lib/visualization"),
+};
