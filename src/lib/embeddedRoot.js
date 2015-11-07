16c11add02af3c95acf88d2b13a69377100a87fb
Heribert Schütz <hs@webxcerpt.com>
2015-11-07 17:15:26 +0100

    Rewrote embedding API (to handle derived state).

--- src/lib/embeddedRoot.js
+++ src/lib/embeddedRoot.js
@@ -1,14 +1,59 @@
 var React = require("react");
 
+function delay(fn) {
+	setTimeout(fn, 1);
+}
+
+// for JSON-style values
+function deepEqual(x, y) {
+	if (x === y)
+		return true;
+	if (x == null || y == null)
+		return false;
+	const c = x.constructor;
+	if (y.constructor !== c)
+		return false;
+	switch (c) {
+	case Array: {
+		const l = x.length;
+		if (y.length != l)
+			return false;
+		for (var i = 0; i < l; i++)
+			if (!deepEqual(x[i], y[i]))
+				return false;
+	}
+	case Object: {
+		for (const i in x)
+			if (!(i in y && deepEqual(x[i], y[i])))
+				return false;
+		for (const i in y)
+			if (!(i in x))
+				return false;
+	}
+	default:
+		return false;
+	}
+}
+
 var EmbeddedRoot = React.createClass({
+	getInitialState() {
+		const {embeddingAPI} = this.props;
+		embeddingAPI.inward = config => delay(() => {
+			if (!deepEqual(this.state.config, config))
+				this.state.config = config;
+		});
+		// ### remove embeddingAPI.inward upon unload?
+		return {config: embeddingAPI.config};
+	},
 	render() {
 		const {type, embeddingAPI, initialCtxProvider} = this.props;
-		var node = type.makeNode({
+		const ctx = {
 			...initialCtxProvider(),
-			value: embeddingAPI.state,
-			updateTo: newValue => embeddingAPI.state = newValue,
-		});
-		embeddingAPI.stateChanged = () => this.forceUpdate();
+			value: this.state.config,
+			updateTo: newValue => this.setState({config: newValue}),
+		};
+		var node = type.makeNode(ctx);
+		delay(() => embeddingAPI.outward(ctx));
 		return node.render();
 	},
 });
