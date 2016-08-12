031448042444e868f011b25270d18e9a320a6d41
Heribert Schütz <hs@webxcerpt.com>
2016-08-12 13:17:09 +0200

    Moved renderer to separate file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import {CGroup, CSelect, CText, CNumeric, cref} from "./opencpq.js"
+import renderer from "./renderers/ugly-renderer.js";
 
 /*
 const tShirt = config`[
@@ -69,89 +70,6 @@ const tShirt = CGroup([
     ])}
   ],
 ]);
-
-const modeColors = {
-  "default": "blue",
-  normal: "black",
-  warning: "yellow",
-  error: "red",
-  hidden: "white",
-}
-
-const renderer = {
-  visitGroup({members}) {
-    console.log("render group");
-    return (
-      <dl>
-        {
-          [].concat(...members.map(({tag, props = {}, node}) => {
-            const {label = tag} = props;
-            return [
-              <dt key={`dt-${tag}`}>{label}</dt>,
-              <dd key={`dd-${tag}`}>{node.accept(this)}</dd>
-            ];
-          }))
-        }
-      </dl>
-    );
-  },
-  visitSelect({choices, isUserInput, choice, fullChoice: {resolvedMode}, detail, choose, unset}) {
-    console.log("render select");
-    return (
-      <div>
-        <span>
-          <select
-            value={choice}
-            onChange={event => choose(event.target.value)}
-            style={{color: modeColors[resolvedMode]}}
-          >
-            {
-              choices.map(({tag, resolvedMode, props: {label = tag} = {}}) => {
-                console.log("render choice", tag);
-                return (
-                  resolvedMode === "hidden" ? null :
-                  <option
-                    key={`option-${tag}`}
-                    value={tag}
-                    style={{color: modeColors[resolvedMode]}}
-                  >
-                    {label}{" "}[{resolvedMode}]
-                  </option>
-                );
-              })
-            }
-          </select>
-          {isUserInput &&
-            <button onClick={unset}>unset</button>
-          }
-        </span>
-        {console.log("render sel. detail"), detail && detail.accept(this)}
-      </div>
-    );
-  },
-  visitPrimitive({value = "", updateTo}) {
-    console.log("render primitive");
-    return (
-      <input
-        type="text"
-        value={value}
-        onChange={event => updateTo(event.target.value)}
-      />
-    );
-  },
-  visitUnit() {
-    console.log("render unit");
-    return null;
-  },
-  visitConfig() {
-    console.log("render config");
-    return (
-      <span style={{color: "red", fontWeight: "bold", border: "2px solid red"}}>
-        (renderer missing)
-      </span>
-    );
-  },
-};
 
 class Configurator extends React.Component {
   constructor(props, context) {
---
+++ src/renderers/ugly-renderer.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+const modeColors = {
+  "default": "blue",
+  normal: "black",
+  warning: "yellow",
+  error: "red",
+  hidden: "white",
+}
+
+export default {
+  visitGroup({members}) {
+    console.log("render group");
+    return (
+      <dl>
+        {
+          [].concat(...members.map(({tag, props = {}, node}) => {
+            const {label = tag} = props;
+            return [
+              <dt key={`dt-${tag}`}>{label}</dt>,
+              <dd key={`dd-${tag}`}>{node.accept(this)}</dd>
+            ];
+          }))
+        }
+      </dl>
+    );
+  },
+  visitSelect({choices, isUserInput, choice, fullChoice: {resolvedMode}, detail, choose, unset}) {
+    console.log("render select");
+    return (
+      <div>
+        <span>
+          <select
+            value={choice}
+            onChange={event => choose(event.target.value)}
+            style={{color: modeColors[resolvedMode]}}
+          >
+            {
+              choices.map(({tag, resolvedMode, props: {label = tag} = {}}) => {
+                console.log("render choice", tag);
+                return (
+                  resolvedMode === "hidden" ? null :
+                  <option
+                    key={`option-${tag}`}
+                    value={tag}
+                    style={{color: modeColors[resolvedMode]}}
+                  >
+                    {label}{" "}[{resolvedMode}]
+                  </option>
+                );
+              })
+            }
+          </select>
+          {isUserInput &&
+            <button onClick={unset}>unset</button>
+          }
+        </span>
+        {console.log("render sel. detail"), detail && detail.accept(this)}
+      </div>
+    );
+  },
+  visitPrimitive({value = "", updateTo}) {
+    console.log("render primitive");
+    return (
+      <input
+        type="text"
+        value={value}
+        onChange={event => updateTo(event.target.value)}
+      />
+    );
+  },
+  visitUnit() {
+    console.log("render unit");
+    return null;
+  },
+  visitConfig() {
+    console.log("render config");
+    return (
+      <span style={{color: "red", fontWeight: "bold", border: "2px solid red"}}>
+        (renderer missing)
+      </span>
+    );
+  },
+};
